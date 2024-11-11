87e325d7a75608f8224467c8c62c661d22c8bcaf
William Chen <wiredwilliamchen@outlook.com>
2024-11-11 12:03:52 -0500

    Changed to method & added while loop win condition

--- pair_programming.py
+++ pair_programming.py
@@ -1,21 +1,33 @@
 
 import random
 
+def check_duplicate(lis:list):
+    dup = [1,1,1]
+    for i in range(len(lis)):
+        for j in range(len(lis)):
+            if not i == j:
+                if lis[i] == lis[j]:
+                    dup[i] = -1
+                    dup[j] = -1
+    return dup
 
-three_dice = random.choices(range(6), k = 3)
-fixed = [1,1,1]
+
+three_dice = random.choices(range(1,7), k = 3)
 
 player1_score = 0
 player2_score = 0
 
+while not player1_score or player2_score >= 20:
+    
 
-for i in range(three_dice):
-    for j in range(three_dice):
-        if not i == j:
-            if three_dice[i] == three_dice[j]:
-                fixed[i] = -1
-                fixed[j] = -1
+    fin_roll = ""
+    while not fin_roll.lower == "done":
+        if check_duplicate(three_dice)[0] == 1:
+            three_dice[0] = random.sample(range(1,7), k = 1)
+        if check_duplicate(three_dice)[1] == 1:
+            three_dice[1] = random.sample(range(1,7), k = 1)
+        if check_duplicate(three_dice)[2] == 1:
+            three_dice[2] = random.sample(range(1,7), k = 1)
 
-
-
-
+        fin_roll = input("Type done if happy with rolls")
+    
