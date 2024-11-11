b6ef1a1a19d372f9e5f543ee97245bb65dc5788e
William Chen <wiredwilliamchen@outlook.com>
2024-11-11 12:14:20 -0500

    Bug fixed turn control and method titling

--- pair_programming.py
+++ pair_programming.py
@@ -11,35 +11,44 @@ def check_duplicate(lis:list):
                     dup[j] = -1
     return dup
 
-
-three_dice = random.choices(range(1,7), k = 3)
+three_dice = [0,0,0]
 
 player1_score = 0
 player2_score = 0
 
 turn = 1
 
-while not player1_score >= 20 or player2_score >= 20:
+while not player1_score or player2_score >= 20:
     fin_roll = ""
     while not fin_roll.lower == "done":
+        three_dice = random.choices(range(1,7), k = 3)
+        
         print("Current rolls: ", three_dice)
-        if check_duplicate(three_dice)[0] == 1:
-            three_dice[0] = random.sample(range(1,7), k = 1)
-        if check_duplicate(three_dice)[1] == 1:
-            three_dice[1] = random.sample(range(1,7), k = 1)
-        if check_duplicate(three_dice)[2] == 1:
-            three_dice[2] = random.sample(range(1,7), k = 1)
+        print(f"It is currently",end=" ")
+        if turn == 1:
+            print("Player 1's turn")
+        elif turn == -1:
+            print("Player 2's turn")
 
         if not check_duplicate(three_dice).count == 3:
-            fin_roll = input("Type done if happy with rolls")
+            fin_roll = input("Only type \"done\" if happy with rolls\n")
         else:
-            print("You have ")
-    if turn == 1:
+            print("You have TUPLED OUT (rolling three of the same)\nYour scored 0 points this round")
+        
+        if check_duplicate(three_dice)[0] == 1:
+            three_dice[0] = random.choice(range(1,7))
+        if check_duplicate(three_dice)[1] == 1:
+            three_dice[1] = random.choice(range(1,7))
+        if check_duplicate(three_dice)[2] == 1:
+            three_dice[2] = random.choice(range(1,7))
+
+        
+    if turn == 1 and not check_duplicate(three_dice).count == 3:
         player1_score = three_dice[0] + three_dice[1] + three_dice[2]
-        turn = 2
-    elif turn == 2:
+    elif turn == -1 and not check_duplicate(three_dice).count == 3:
         player2_score = three_dice[0] + three_dice[1] + three_dice[2]
-        turn = 1
+        
+    turn *= -1
 
     print(f"Current Scores\nPlayer 1: {player1_score}\nPlayer 2: {player2_score}\n")
 
