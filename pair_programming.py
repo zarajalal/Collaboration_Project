cdd71c9b5a6c22ea0feb5836a498505ed59cecad
William Chen <wiredwilliamchen@outlook.com>
2024-11-11 12:00:26 -0500

    Defined 3 random dice & code for identifing duplicate dice

--- pair_programming.py
+++ pair_programming.py
@@ -1,2 +1,21 @@
 
 import random
+
+
+three_dice = random.choices(range(6), k = 3)
+fixed = [1,1,1]
+
+player1_score = 0
+player2_score = 0
+
+
+for i in range(three_dice):
+    for j in range(three_dice):
+        if not i == j:
+            if three_dice[i] == three_dice[j]:
+                fixed[i] = -1
+                fixed[j] = -1
+
+
+
+
