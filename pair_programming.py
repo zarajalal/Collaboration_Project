ccb0886cf71bc3874aa280c9c43f7b10e4be4220
William Chen <wiredwilliamchen@outlook.com>
2024-11-11 12:11:45 -0500

    Added score counting & printed text

--- pair_programming.py
+++ pair_programming.py
@@ -17,11 +17,12 @@ three_dice = random.choices(range(1,7), k = 3)
 player1_score = 0
 player2_score = 0
 
-while not player1_score or player2_score >= 20:
-    
+turn = 1
 
+while not player1_score >= 20 or player2_score >= 20:
     fin_roll = ""
     while not fin_roll.lower == "done":
+        print("Current rolls: ", three_dice)
         if check_duplicate(three_dice)[0] == 1:
             three_dice[0] = random.sample(range(1,7), k = 1)
         if check_duplicate(three_dice)[1] == 1:
@@ -29,5 +30,20 @@ while not player1_score or player2_score >= 20:
         if check_duplicate(three_dice)[2] == 1:
             three_dice[2] = random.sample(range(1,7), k = 1)
 
-        fin_roll = input("Type done if happy with rolls")
-    
+        if not check_duplicate(three_dice).count == 3:
+            fin_roll = input("Type done if happy with rolls")
+        else:
+            print("You have ")
+    if turn == 1:
+        player1_score = three_dice[0] + three_dice[1] + three_dice[2]
+        turn = 2
+    elif turn == 2:
+        player2_score = three_dice[0] + three_dice[1] + three_dice[2]
+        turn = 1
+
+    print(f"Current Scores\nPlayer 1: {player1_score}\nPlayer 2: {player2_score}\n")
+
+if player1_score >= 20:
+    print("PLAYER 1 WON")
+elif player2_score >= 20:
+    print("PLAYER 2 WON")
